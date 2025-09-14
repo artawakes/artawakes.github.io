93a50880b0f77a6a7a81eb56f761d8452bc58718
Artawakes <Midnightblue.air>
2025-09-13 22:00:55 -0700

    Update Forkright project: reorganize images, add hero image, remove duplicate images, and improve styling

--- projects/forkright-brand/project-data.js
+++ projects/forkright-brand/project-data.js
@@ -27,13 +27,17 @@ export default {
     featureImage: {
         src: "./Images/d0816a0f-ef41-4cd8-8e09-53d553af2638.jpg",
         alt: "Forkright Brand",
-        caption: "Recipe management and meal planning application design"
+        caption: ""
     },
     
     // Process steps
     process: [
         {
             title: "Research & Discovery",
+            heroImage: {
+                src: "./Images/5674fe28-9431-4db5-bd2d-50c4faba5e99_rw_1920.jpg",
+                alt: "Design Process Hero"
+            },
             content: [
                 "Conducted comprehensive research on recipe management and meal planning needs",
                 "Interviewed home cooks and food enthusiasts to understand workflow requirements",
@@ -41,12 +45,44 @@ export default {
             ],
             images: [
                 {
-                    src: "./Images/2ca906a8-d696-4e6e-ad83-12f4734b16a9_rw_1920.jpeg",
-                    alt: "Brand Guidelines"
+                    src: "./Images/8bf92325-b654-4385-9060-3a08222d4cfb_rw_1920.jpg",
+                    alt: "Research & Discovery"
                 },
                 {
-                    src: "./Images/a34600e1-77f0-4a93-9819-342501b70a2c_rw_1200.jpeg",
-                    alt: "Color System"
+                    src: "./Images/a4a0eb72-46bc-45bd-b531-f31824cecb9c_rw_1920.jpg",
+                    alt: "User Research Insights"
+                },
+                {
+                    src: "./Images/23e14ad3-8b5a-4fad-bb27-4ae91827ddf8_rw_1920.jpg",
+                    alt: "Ideation & Sketching"
+                },
+                {
+                    src: "./Images/44ebc16f-da73-47a7-8410-0bfdc06c0e5d_rw_1920.jpg",
+                    alt: "Concept Development"
+                },
+                {
+                    src: "./Images/00114a89-837e-45b4-94f6-cb0c7b460b6d_rw_3840.jpg",
+                    alt: "Wireframing & Prototyping"
+                },
+                {
+                    src: "./Images/134bee43-6c49-43f7-82e9-e2b1ee55a659_rw_1920.jpg",
+                    alt: "User Testing & Feedback"
+                },
+                {
+                    src: "./Images/397e9d9b-97b0-4028-8915-8052c7436cb0_rw_1920.jpg",
+                    alt: "Iterative Design"
+                },
+                {
+                    src: "./Images/489960d8-b02d-4d51-b78d-4cf3919a45c0_rw_3840.jpg",
+                    alt: "Design Refinement"
+                },
+                {
+                    src: "./Images/d614a216-5058-4080-9591-571cc6ac3e58_rw_3840.jpg",
+                    alt: "Final Design Presentation"
+                },
+                {
+                    src: "./Images/9c3cc54d-6604-4925-98ea-d6af1b6a4b61_rw_1920.jpg",
+                    alt: "Brand Strategy & Vision"
                 }
             ],
             outcomes: [
@@ -65,13 +101,53 @@ export default {
             ],
             images: [
                 {
-                    src: "./Images/ff01f81e-8770-4625-abe0-c28d5c7059a9_rw_1200.jpeg",
-                    alt: "Typography"
+                    src: "./Images/4ea7b526-c6a6-49b5-a1c5-f4f9690fbb8f_rw_1920.jpg",
+                    alt: "Brand Identity Design"
+                },
+                {
+                    src: "./Images/5cec95b6-fcdf-4e7b-b19d-6fe96583e797_rw_1200.jpeg",
+                    alt: "Visual System"
+                },
+                {
+                    src: "./Images/82fa3801-7932-4703-8d06-cf8fa76ccd1c_rw_1200.jpeg",
+                    alt: "Logo Variations"
+                },
+                {
+                    src: "./Images/227d6868-91b0-4b9f-9b42-c4789b069d91_rw_1200.jpeg",
+                    alt: "Color Palette"
+                },
+                {
+                    src: "./Images/040840d6-4031-4634-8e65-7da5936d5f93_rw_1200.jpeg",
+                    alt: "Icon Library"
+                },
+                {
+                    src: "./Images/b97ba606-8191-469b-9b21-8f1e95dc99f3_rw_1920.jpg",
+                    alt: "Brand Applications"
+                },
+                {
+                    src: "./Images/be1887c3-3a78-46be-b594-7f5beb6a0d2c_rw_1200.jpeg",
+                    alt: "Marketing Materials"
+                },
+                {
+                    src: "./Images/e1975560-8825-4716-88a5-c39ee11385a6_rw_1920.jpg",
+                    alt: "Brand Experience"
+                },
+                {
+                    src: "./Images/f0a2bab9-a8cc-4fdf-9952-c1f3ccd6326c_rw_1200.jpeg",
+                    alt: "Social Media Design"
                 },
                 {
                     src: "./Images/18c19358-c18d-43ba-8b35-5374f84c6dd7_rw_1200.jpeg",
                     alt: "Component Library"
-                }
+                },
+                {
+                    src: "./Images/2ca906a8-d696-4e6e-ad83-12f4734b16a9_rw_1920.jpeg",
+                    alt: "Brand Guidelines"
+                },
+                {
+                    src: "./Images/a34600e1-77f0-4a93-9819-342501b70a2c_rw_1200.jpeg",
+                    alt: "Color System"
+                },
             ],
             outcomes: [
                 "Complete brand identity system",
